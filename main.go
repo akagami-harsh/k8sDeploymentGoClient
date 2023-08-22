0f366c3bb8d28eb99d98fa49498d0a2a69585b5c
harshvir potpose <hpotpose62@gmail.com>
2023-08-22 23:26:12 +0530

    adding update functionality so that means if deployment is already created and we run our client it will update the existing deployment

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 
 	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -69,7 +70,19 @@ func deploy(ctx context.Context, client *kubernetes.Clientset) (map[string]strin
 		return nil, fmt.Errorf("Unrecognized type: %s\n", groupVersionKind)
 	}
 
-	deploymentResponse, err := client.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
+	_, err = client.AppsV1().Deployments("default").Get(ctx, deployment.Name, metav1.GetOptions{})
+	if err != nil && errors.IsNotFound(err) {
+		deploymentResponse, err := client.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
+		if err != nil {
+			return nil, fmt.Errorf("deployment error %s", err)
+		}
+		return deploymentResponse.Spec.Template.Labels, nil
+
+	} else if err != nil && errors.IsNotFound(err) {
+		return nil, fmt.Errorf("deployment error: %s", err)
+	}
+
+	deploymentResponse, err := client.AppsV1().Deployments("default").Update(ctx, deployment, metav1.UpdateOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("deployment error %s", err)
 	}
